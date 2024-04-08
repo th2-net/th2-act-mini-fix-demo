d2f9fd0de0a5816bd6c8b21d1dfa9d8fbd395772
Oleg Smelov <oleg.smelov@exactpro.com>
2024-04-08 14:36:30 +0400

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,16 @@
 plugins {
     id("application")
     id("java")
-
     kotlin("jvm") version "1.8.22"
-
-    val th2PluginVersion = "0.0.4"
-    id("com.exactpro.th2.gradle.base") version th2PluginVersion
-    id("com.exactpro.th2.gradle.component") version th2PluginVersion
+    id("com.exactpro.th2.gradle.component") version "0.0.4"
 }
 
 kotlin {
     jvmToolchain(11)
 }
 
-allprojects {
-    val version = project.properties["release_version"].toString()
-    val suffix = project.properties["version_suffix"]?.toString() ?: ""
-    this.group = "com.exactpro.th2"
-    this.version = version + if (suffix.isEmpty()) "" else "-$suffix"
-}
+group = "com.exactpro.th2"
+version = project.properties["release_version"].toString()
 
 configurations.all {
     resolutionStrategy.cacheChangingModulesFor(0, "seconds")
@@ -28,7 +20,7 @@ configurations.all {
 dependencies {
     implementation("com.exactpro.th2:common:5.10.0-dev")
     implementation("com.exactpro.th2:grpc-common:4.4.0-dev")
-    implementation("com.exactpro.th2:grpc-hand:3.0.0-RM-84612-8544324408-SNAPSHOT")
+    implementation("com.exactpro.th2:grpc-hand:3.0.0-RM-84612-8598332645-501bbe0-SNAPSHOT")
     implementation("com.exactpro.th2:hand:5.0.0-RM-84612-no-log4j-8545363969-62a26ef-SNAPSHOT")
     implementation("com.exactpro.th2:act-gui-core:3.0.0-RM-84612-8552406828-ac72781-SNAPSHOT")
     implementation("com.exactpro.th2:grpc-act-uiframework-win-demo:1.3.0-RM-84612-8544485219-ed352c1-SNAPSHOT")
